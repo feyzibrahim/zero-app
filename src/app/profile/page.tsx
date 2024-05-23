aa72a2d78afcac6593c21583138ed7962178fece
Feyz Ibrahim <faizashrafkk@gmail.com>
2024-05-23 14:03:01 +0530

    feat: logout complete

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,7 +1,34 @@
+"use client";
+
+import axios from "axios";
+import { useRouter } from "next/navigation";
+import toast from "react-hot-toast";
+
 export default function ProfilePage() {
+  const router = useRouter();
+
+  const logout = async () => {
+    try {
+      await axios.get("/api/users/logout");
+      toast.success("Logged out successfully");
+      router.push("/login");
+    } catch (error: any) {
+      console.log(error.message);
+      toast.error("Logout Failed");
+    }
+  };
+
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <h1>Profile Page</h1>
+      <hr />
+
+      <button
+        onClick={logout}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow"
+      >
+        Logout
+      </button>
     </div>
   );
 }
